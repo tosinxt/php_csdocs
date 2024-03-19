74501c5c0d04915b96e216ef6fbfb05dcf1677b1
tosinxt <111095124+tosinxt@users.noreply.github.com>
2024-03-19 14:46:45 +0100

    ndjlcnd
    dm a.md

---
+++ insert.php
@@ -0,0 +1,12 @@
+<html>
+    <head></head>
+    <body>
+        <form action="insert.php" method="post">
+        <label for="">Book Name</label><input type="text" name=""><br>
+        <label for="">Book Author</label><input type="Author" name=""><br>
+        <label for="">Book Publisher</label><input type="Publisher" name=""><br>
+        <label for="">Publication Year</label><input type="Year" name=""><br>
+        <input type="submit" name="submit" value="register Book"><br>
+        </form>
+    </body>
+</html>
