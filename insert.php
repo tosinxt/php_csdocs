624e143f0805a41249b5f8d9971883753f79a28f
tosinxt <allioluwatosin8@gmail.com>
2024-03-19 14:56:44 +0100

    updated db

--- insert.php
+++ insert.php
@@ -1,3 +1,30 @@
+<?php
+// Establish database connection
+$dbconnect = mysqli_connect('localhost', 'root', '', 'book') or die('Failed to connect to the database');
+
+
+if(isset($_POST['submit'])) {
+
+    $book_name =$_POST['book_name'];
+    $book_author =$_POST['book_author'];
+    $book_publisher =$_POST['book_publisher'];
+    $publication_year =$_POST['publication_year'];
+
+    
+    $query = "INSERT INTO books (book_name, book_author, book_publisher, publication_year) 
+              VALUES ('$book_name', '$book_author', '$book_publisher', '$publication_year')";
+    
+    if(mysqli_query($dbconnect, $query)) {
+        echo "Book inserted successfully.";
+    } else {
+        echo "Error: " . mysqli_error($dbconnect);
+    }
+}
+
+
+mysqli_close($dbconnect);
+?>
+
 <html>
     <head></head>
     <body>
